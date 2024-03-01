477ad0635185096616202f8ff4b5bd91789ae025
egasa21 <esaputra168@gmail.com>
2024-03-01 15:37:21 +0700

    feat: internet screen

--- lib/presentation/screens/internet_screen.dart
+++ lib/presentation/screens/internet_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:internet_feature/data/internet_package.dart';
+import 'package:internet_feature/presentation/screens/transaction_history_screen.dart';
 import 'package:internet_feature/presentation/widgets/expandable_card_widget.dart';
+import 'package:internet_feature/presentation/widgets/my_app_bar.dart';
+import 'package:intl/intl.dart'; // Import intl package
 
 class InternetScreen extends StatefulWidget {
   const InternetScreen({Key? key}) : super(key: key);
@@ -9,43 +13,16 @@ class InternetScreen extends StatefulWidget {
 }
 
 class _InternetScreenState extends State<InternetScreen> {
-  // final bool _isExpanded = false;
+  bool _isAllSelected = false;
+  List<bool> _cardSelection =
+      List.generate(internetPackages.length, (index) => false);
+  double _totalPrice = 0.0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(kToolbarHeight),
-          child: Container(
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.1),
-                  spreadRadius: 0,
-                  blurRadius: 2,
-                  offset: const Offset(0, 3),
-                )
-              ],
-            ),
-            child: AppBar(
-              leading: IconButton(
-                onPressed: () => Navigator.of(context).pop(),
-                icon: const Icon(Icons.arrow_back_ios),
-                color: const Color(0xff3E414C),
-              ),
-              title: const Text(
-                "Internet",
-                style: TextStyle(
-                  fontFamily: "Roboto",
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              centerTitle: true,
-            ),
-          ),
-        ),
+        appBar: const MyAppBar(title: "Internet"),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -108,46 +85,99 @@ class _InternetScreenState extends State<InternetScreen> {
                       height: 24.0,
                       width: 24.0,
                       child: Checkbox(
-                        value: false,
-                        onChanged: (bool? newValue) {},
+                        value: _isAllSelected,
+                        onChanged: (bool? newValue) {
+                          setState(() {
+                            _isAllSelected = newValue ?? false;
+                            _cardSelection = List.generate(
+                                internetPackages.length,
+                                (index) => _isAllSelected);
+                            _updateTotalPrice();
+                          });
+                        },
                       ),
                     ),
                   ],
                 ),
               ),
               const SizedBox(height: 16),
-              const Expanded(
+              Expanded(
                 child: SingleChildScrollView(
                   child: Column(
                     children: [
-                      ExpandableCardWidget(),
-                      ExpandableCardWidget(),
-                      MyCustomIconButton()
+                      ...List.generate(internetPackages.length, (index) {
+                        return ExpandableCardWidget(
+                          price: internetPackages[index].price,
+                          dueDate: internetPackages[index].dueDate,
+                          provider: internetPackages[index].provider,
+                          idPelanggan: internetPackages[index].idPelanggan,
+                          paketLayanan: internetPackages[index].paketLayanan,
+                          isChecked: _cardSelection[index],
+                          onCheckboxChanged: (isChecked) {
+                            setState(() {
+                              _cardSelection[index] = isChecked ?? false;
+                              _isAllSelected =
+                                  _cardSelection.every((value) => value);
+                              _updateTotalPrice();
+                            });
+                          },
+                        );
+                      }),
+                      MyCustomIconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  const TransactionHistoryScreen(),
+                            ),
+                          );
+                        },
+                      ),
                     ],
                   ),
                 ),
               ),
+
               const SizedBox(height: 16), // Add padding here
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
                 child: Column(
                   children: [
                     Row(
                       children: [
-                        Icon(Icons.receipt_long_outlined),
-                        SizedBox(width: 8), // Add space between Icon and Text
-                        Text(
+                        Image.asset(
+                          "assets/images/icons/ic_bill.png",
+                          width: 24,
+                          height: 24,
+                        ),
+                        const SizedBox(
+                            width: 8), // Add space between Icon and Text
+                        const Text(
                           "Total Payment",
                           style: TextStyle(
                               fontSize: 16, fontWeight: FontWeight.w500),
                         ),
-                        SizedBox(width: 8), // Add space between Text and Price
-                        Spacer(), // Fill available space
-                        Text("Rp450.000")
+                        const SizedBox(
+                            width: 8), // Add space between Text and Price
+                        const Spacer(), // Fill available space
+                        Text(
+                          NumberFormat.currency(
+                            locale: 'id',
+                            symbol: 'Rp',
+                            decimalDigits: 0,
+                          ).format(
+                              _totalPrice), // Display total price in rupiah format
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF244091),
+                          ),
+                        )
                       ],
                     ),
-                    SizedBox(height: 16), // Add padding here
-                    MyCustomButton(onPressed: emptyFunction),
+                    const SizedBox(height: 16), // Add padding here
+                    const MyCustomButton(onPressed: emptyFunction),
                   ],
                 ),
               ),
@@ -157,20 +187,38 @@ class _InternetScreenState extends State<InternetScreen> {
       ),
     );
   }
+
+  void _updateTotalPrice() {
+    _totalPrice = 0.0;
+    for (int i = 0; i < internetPackages.length; i++) {
+      if (_cardSelection[i]) {
+        _totalPrice += internetPackages[i].price;
+      }
+    }
+  }
 }
 
 class MyCustomIconButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
   const MyCustomIconButton({
-    super.key,
-  });
+    Key? key,
+    required this.onPressed,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.fromLTRB(16.0, 20.0, 16.0, 20.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [Text("Transaction History"), Icon(Icons.arrow_forward_ios)],
+    return InkWell(
+      onTap: onPressed,
+      child: const Padding(
+        padding: EdgeInsets.fromLTRB(16.0, 20.0, 16.0, 20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Transaction History"),
+            Icon(Icons.arrow_forward_ios),
+          ],
+        ),
       ),
     );
   }
