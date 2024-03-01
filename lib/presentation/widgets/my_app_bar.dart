25fa32fe694d80a5856b35e7b05a1fbae852562c
egasa21 <esaputra168@gmail.com>
2024-03-01 15:39:27 +0700

    feat: custom app bar

---
+++ lib/presentation/widgets/my_app_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const MyAppBar({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            spreadRadius: 0,
+            blurRadius: 2,
+            offset: const Offset(0, 3),
+          )
+        ],
+      ),
+      child: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const Icon(Icons.arrow_back_ios),
+          color: const Color(0xff3E414C),
+        ),
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontFamily: "Roboto",
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
